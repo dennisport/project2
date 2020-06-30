e4cde5df694a8754932d301c3a9d624ff59ba3db
Dennis <Dennis.portillo@yale.edu>
2020-06-30 12:31:44 -0700

    user side added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   resources :communities
   resources :posts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/users/:id/follow', to: "users#follow", as: "follow_user"
   post '/users/:id/unfollow', to: "users#unfollow", as: "unfollow_user"
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_203625) do
+ActiveRecord::Schema.define(version: 2020_06_29_214124) do
 
   create_table "communities", force: :cascade do |t|
     t.string "description"
@@ -37,10 +37,11 @@ ActiveRecord::Schema.define(version: 2020_06_29_203625) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
-    t.text "bio"
+    t.string "password_digest"
+    t.string "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
