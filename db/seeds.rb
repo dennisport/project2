b2d6f17d886033c13a464a6d80e562d2173a1dda
adilgondal1 <adil.gondal@yale.edu>
2020-07-01 12:28:31 -0400

    post crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Community.create(name: "Sports", description: "All sports stuff goes here!")
+# Community.create(name: "Other", description: "Miscellaneous stuffs")
+
+Post.create(name: "NBA is back!", description: "NBA is set to return on july 30th!", community_id: 1)
+
